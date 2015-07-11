e42830c22dc4a276ff93b369a55ba1efcf29e49d
Fabien Lavocat <fabien.lavocat@live.com>
2015-07-11 15:32:05 -0700

    Add the source code for the library and example

---
+++ firmware/example/RunLoops.ino
@@ -0,0 +1,21 @@
+// Use this include for the Web IDE:
+#include "Loop/Loop.h"
+
+Loop cloudSyncTimeLoop(ONE_DAY_SECONDS);
+
+void setup(void) {
+	
+}
+
+void loop(void) {
+	cloudSyncTimeLoop.run(&cloudSyncTime);
+	
+	delay(1000);
+}
+
+// Synchronize the time with the Particle Cloud.
+// This happens automatically when the device connects to the Cloud.
+// However, if your device runs continuously for a long time, you may want to synchronize once per day or so.
+void cloudSyncTime() {
+    Spark.syncTime();
+}
